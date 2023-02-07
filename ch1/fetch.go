4d09676a42ca09a65ab9414c0515855c78ba7f26
fhirt <tiehir@tie.ch>
2023-02-07 14:26:38 +0100

    fetch

---
+++ ch1/fetch.go
@@ -0,0 +1,20 @@
+// Fetch prints the content found at a URL.
+package ch1
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func Fetch() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		io.Copy(os.Stdout, resp.Body)
+	}
+}
--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"gopl/ch1"
-	"os"
 )
 
 func main() {
-	ch1.Lissajous(os.Stdout)
+	ch1.Fetch()
 }
